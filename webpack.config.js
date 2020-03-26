048e9914c3cb6b73d754895a0e6e7b780a670d46
Joan <joanques17@gmail.com>
2020-03-26 17:47:49 +0100

    webcnfg2

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         master:__dirname + "/src/std/_master-file.js",
         miguel:__dirname + "/src/std/mexposito.js",
         fcastell:__dirname + "/src/std/fcastell.js",
-        jques:__dirname + "/src/std/jques.js"
+        jques:__dirname + "/src/std/jques.js",
         dcastañeda:__dirname + "/src/std/dcastañeda.js",
     },
     output: {
