4f1bbaddbf174d61d407eca55106d54019febd39
Darkstar8217 <dsd00@iesemilidarder.com>
2020-03-26 18:36:50 +0100

    dsalanova

--- src/std/dsalanova.js
+++ src/std/dsalanova.js
@@ -1,1 +1,7 @@
 console.log("Soy Dani Salanova ");
+
+function create (){
+    let boton = document.createElement("BUTTON");
+    boton.innerHTML = "HE SIDO CREADO";
+    document.body.appendChild(boton).getElementById("btn").innerHTML;
+}
