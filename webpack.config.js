10176d70d3b0c16b556679a731ec28ddceff3615
Toni286 <aap02@iesemilidarder.coms>
2020-03-26 17:48:10 +0100

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
         fcastell:__dirname + "/src/std/fcastell.js",
         jques:__dirname + "/src/std/jques.js"
         dcastañeda:__dirname + "/src/std/dcastañeda.js",
+        tamer:__dirname + "/src/std/tamer.js",
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
