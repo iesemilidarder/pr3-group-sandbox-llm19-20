c9f233dcf8c92b3b3601505a75f011ccf5feb147
Javier <jzb00@iesemilidarder.com>
2020-03-26 18:23:25 +0100

    g

--- src/std/jzarco.js
+++ src/std/jzarco.js
@@ -1,1 +1,13 @@
-console.log("Hola soy javi hehe")
+console.log("Hola soy javi hehe");
+
+function function1() {
+    console.log("hola1")
+}
+const function2 = function () {
+    console.log("hola2")
+};
+
+//new version
+const myRender = () =>{
+  console.log("hola3")
+};
