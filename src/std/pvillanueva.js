0259ca7c9497ae0428332147523d304edf4382bd
Pablis369 <pvm00@iesemilidarder.com>
2020-04-02 18:16:43 +0200

    02/04/2020

--- src/std/pvillanueva.js
+++ src/std/pvillanueva.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MasterHeader from "./Components/MasterHeader"
+
 function function1() {
     console.log("Hi! Im Pablo");
 }
 const function2 = function () {
     console.log("Hi! Im Pablo");
 };
-
 const myRender = (container, title) => {
     console.log("arrow function");
     getPlaylist().then(results => {
@@ -16,7 +19,6 @@ const myRender = (container, title) => {
         document.querySelector(container).innerHTML = template;
     });
 };
-
 const mySidebar = (container) => {
     const template = `
         <h1>Title</h1>
@@ -26,7 +28,6 @@ const mySidebar = (container) => {
     `;
     document.querySelector(container).innerHTML = template;
 };
-
 const getPlaylist = async () => {
     //Esperando a que acabe
     let data = await fetch("https://sandbox-mongo.herokuapp.com/api/rest/v1/playlists");
@@ -47,3 +48,7 @@ const getPlaylist = async () => {
 };
 myRender("#myArticle","Pruebas de clase");
 mySidebar(".sidebar");
+
+ReactDOM.render(<React.StrictMode>
+        <MasterHeader/>
+    </React.StrictMode>, document.getElementById("mainContainer"));
