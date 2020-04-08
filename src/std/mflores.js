03aa0c61ded3d658b24a157b28276486e4c843f2
Alberto Soto <alberto.soto@gmail.com>
2020-04-08 12:25:48 +0200

    css valid

--- src/std/mflores.js
+++ src/std/mflores.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import "./mflores.css";
+import "./mflores.css";
 import {MfloresBody, MfloresFooter, MfloresHeader} from "./Components/MfloresMyPage";
 
 const MyFloresPage = () => {
