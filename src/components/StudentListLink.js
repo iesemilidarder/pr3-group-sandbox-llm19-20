2b42f1c137788786a55959a6c9ec26c78648346a
mikinario <mef01@iesemilidarder.com>
2020-04-11 12:05:17 +0200

    Añadido nuestra pagina y su url a la pagina index.html del proyecto entero

--- src/components/StudentListLink.js
+++ src/components/StudentListLink.js
@@ -16,6 +16,7 @@ const StudentListLink = () => {
         {id:12,link: "/msegui.html",title:"Marta"},
         {id:13,link: "/mflores.html",title:"Matias"},
         {id:14,link: "/#",title:"Francesc Gonzalez"},//TODO F. Gonzalez
+        {id:15,link: "/topics.html",title:"Topics"},
     ];
     return <ul>
         {studentList.map((item) => {
