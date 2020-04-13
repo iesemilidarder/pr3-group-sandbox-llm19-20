b52b20cd46340263dc89efdca448f36cc85b2579
matias <msi00@iesemilidarder.com>
2020-04-13 19:02:35 +0200

    header 3

--- src/prj1/components/MyHeader.js
+++ src/prj1/components/MyHeader.js
@@ -4,7 +4,7 @@ const MyHeader = () =>{
     return <div>
         <header className="row">
 
-            <h1 class="col-md-4">Topics Food</h1>
+            <h1 className="col-md-4">Topics Food</h1>
 
             <div  className="col-md-2">
                 <img src="/img/logo.png"  alt="logo" class="col-6"/>
