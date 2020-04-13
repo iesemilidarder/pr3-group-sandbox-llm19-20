f0248d3d030c2d545c7ac28e574fddecdf809346
matias <msi00@iesemilidarder.com>
2020-04-13 18:43:25 +0200

    header 2

--- src/prj1/components/MyHeader.js
+++ src/prj1/components/MyHeader.js
@@ -3,14 +3,14 @@ import React from "react";
 const MyHeader = () =>{
     return <div>
         <header className="row">
-            <header className="row">
+
             <h1 class="col-md-4">Topics Food</h1>
-            </header>
+
             <div  className="col-md-2">
                 <img src="/img/logo.png"  alt="logo" class="col-6"/>
             </div>
-
+        </header>
 
     </div>
-};
-export default MyHeader;
+
+export default MyHeader;}
