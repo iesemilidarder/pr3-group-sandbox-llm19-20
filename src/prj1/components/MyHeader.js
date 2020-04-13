86dd8adf85b94d33663567dd70c8285786d2a536
matias <msi00@iesemilidarder.com>
2020-04-13 18:10:17 +0200

    intento de header

--- src/prj1/components/MyHeader.js
+++ src/prj1/components/MyHeader.js
@@ -3,8 +3,14 @@ import React from "react";
 const MyHeader = () =>{
     return <div>
         <header className="row">
-            <h1>Topics Food</h1>
-        </header>
+            <header className="row">
+            <h1 class="col-md-4">Topics Food</h1>
+            </header>
+            <div  className="col-md-2">
+                <img src="/img/logo.png"  alt="logo" class="col-6"/>
+            </div>
+
+
     </div>
 };
 export default MyHeader;
