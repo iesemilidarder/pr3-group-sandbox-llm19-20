2fc7dee48858256fdd3f0713766a60356583c00e
mikinario <mef01@iesemilidarder.com>
2020-04-15 14:00:58 +0200

    Corregido la dirección del logo en el Header.

--- src/prj1/components/MyHeader.js
+++ src/prj1/components/MyHeader.js
@@ -7,7 +7,7 @@ const MyHeader = () => {
             <h1 style={{fontColor: "#ffffff"}} className="col-md-4">Topics Food</h1>
 
             <input type="text" value="Buscador" className="col-3"/>
-            <img src="/img/logo.png" alt="logo" className="col-5"/>
+            <img src="/images/imgPrj1/logo.png" alt="logo" className="col-5"/>
 
         </header>
 
