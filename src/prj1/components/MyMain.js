5b7d0e45fb1505cb5643b3622418f036dc6c9ac2
mikinario <mef01@iesemilidarder.com>
2020-04-16 02:20:40 +0200

    CHorradas

--- src/prj1/components/MyMain.js
+++ src/prj1/components/MyMain.js
@@ -24,8 +24,8 @@ class MyMain extends React.Component {
         const projects = this.state.projects;
         projects.map(item => {
             if (id === item.id) {
-                let Recipe = <div key={item.id} className="mt-3 mb-4 justify-content-center">
-                        <img src={item.image} alt="food" width="400" height="220" className="center-block"/>
+                let Recipe = <div key={item.id} className="mt-3 mb-4">
+                        <img src={item.image} alt="food" width="400" height="220"/>
                         <h2>{item.title}</h2>
                         <ol>
                             {item.ingredients.map(ing => {
@@ -67,26 +67,21 @@ showRecipe(id) {
         const projects = this.state.projects;
         projects.map(item => {
             if (id === item.id) {
-                return <div className="col-9">
-                    {projects.map(item => {
-                        const Recipe = <div key={item.id} className="mt-3 mb-4 justify-content-center">
-
-                            <img src={item.image} alt="food" width="400" height="220" className="center-block"/>
-
-                            <h2>{item.title}</h2>
-                            <ol>
-                                {item.ingredients.map(ing => {
-                                    return <li>{ing}</li>
-                                })}
-                            </ol>
-                            <a>{item.description} </a>
-                        </div>;
-                        ReactDOM.render(Recipe, document.getElementById("content"))
-                    })}
-                </div>
+                return document.getElementById("content").innerHTML =
+                    `<div key=\"` + item.id + `\" className="mt-3 mb-4 justify-content-center">
+                <img src=\"` + item.image + `\" alt="food" width="400" height="220" className="center-block"/>
+                <h2>` + item.title + `</h2>
+                <ol>
+                    `
+                    + item.ingredients.map(ing => {
+                        return `<li> ` + ing + ` </li>`;
+                    }).join('') + `
+                </ol>
+                <p>` + item.description + ` </p>
+            </div>
+    `
             }
 
-        })
-
+        });
     }
  */
