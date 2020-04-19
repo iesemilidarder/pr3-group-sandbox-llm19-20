9427f88f0cca31ad01c61332836bb8d7445f1166
Daniel <dcs00@iesemilidarder.com>
2020-04-19 20:33:54 +0200

    Daniel Castaneda

---
+++ src/prj3/components/Myheader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Myheader extends React.Component{
+    render(){
+        <h1>Hola, soy un componente</h1>
+    };
+};
---
+++ src/prj3/game.js
@@ -0,0 +1,14 @@
+function App() {
+    let
+
+    return(`
+        <div className="App"> 
+            <header className="App-header">
+                <h1>Hola, perola</h1>
+            </header>
+        </div>
+        `
+    );
+}
+
+export default App;
