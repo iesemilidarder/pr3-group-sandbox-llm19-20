631047b7aabae7fedad1030bf582c34998937261
Darkstar8217 <dsd00@iesemilidarder.com>
2020-04-20 19:24:03 +0200

    dani s

--- src/prj3/src/Juegos.js
+++ src/prj3/src/Juegos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Myfooter from "./components/Myfooter";
-import Recomendados from "./components/Recomendados";
+import Recomendados from "../components/Recomendados";
 import "./juegos.css"
 
 function App(){
