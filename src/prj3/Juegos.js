3389e8e6f98f14eb9657d6f2f37b9bc9c5f4dead
Pablis369 <pvm00@iesemilidarder.com>
2020-04-20 17:42:07 +0200

    20/04/2020

--- src/prj3/Juegos.js
+++ src/prj3/Juegos.js
@@ -1,2 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Myfooter from "./components/Myfooter";
+
+const wrapper = document.getElementById("main");
+wrapper ? ReactDOM.render(<Myfooter/>, wrapper) : false;
---
+++ src/prj3/components/Myfooter.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Myfooter = () => {
+    return
+    <div>
+        <footer className="row"/>
+    </div>
+};
+export default Myfooter;
