6c065f07f4677fcf8fedc40aa05dc26791e5a285
Daniel <dcs00@iesemilidarder.com>
2020-04-20 19:34:45 +0200

    Daniel Castaneda

--- src/prj3/src/Juegos.js
+++ src/prj3/src/Juegos.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Myfooter from "./components/Myfooter";
-import Recomendados from "../components/Recomendados";
+import MyFooter from "/src/prj3/components/MyFooter";
+import Recomendados from "/src/prj3/components/Recomendados";
+import MyHeader from "/src/prj3/components/MyHeader";
+import MyNav from "/src/prj3/components/Mynav";
 import "./juegos.css"
 
 function App(){
     return(
         <div>
+            <MyHeader><MyNav/><MyForm/></MyHeader>
             <Recomendados/>
             <Myfooter/>
         </div>
