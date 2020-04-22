a0a3f1f44f3025940b0f22879f04be6d286c4db3
Daniel <dcs00@iesemilidarder.com>
2020-04-22 21:15:23 +0200

    Daniel Castaneda

--- src/prj3/components/MyNav.js
+++ src/prj3/components/MyNav.js
@@ -2,21 +2,24 @@ import React from "react";
 
 const MyNav = () => {
     return <main className="row">
-        <article className="col-md-10">
-            <h1>Hello world</h1>
-            <section>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, animi cum deleniti ea harum illum
-                laborum
-                libero maxime minus molestias, nam nobis officia praesentium ratione sequi soluta, tenetur unde
-                voluptatibus!
-            </section>
-        </article>
-        <div className="col-md-10" id="cont"/>
-        <aside className="col-md-2">
-            <h1>Esto es un right aside</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur at commodi cum, dignissimos
-                doloremque ea eius eveniet expedita illo inventore iste nam necessitatibus odio porro totam, ullam,
-                voluptates voluptatibus.</p>
-        </aside>
+        <nav className="col-md-6 navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">My web</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     </main>
 };
 
