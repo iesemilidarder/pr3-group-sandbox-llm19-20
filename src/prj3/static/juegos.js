51fc4faa7f2f2f7d85fa44965d62280f96c218e8
Daniel <dcs00@iesemilidarder.com>
2020-04-22 16:52:31 +0200

    Daniel Castaneda

--- src/prj3/static/juegos.js
+++ src/prj3/static/juegos.js
@@ -2,12 +2,12 @@ console.log("hey que onda");
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Recomendados from "./components/Recomendados";
-import MyHeader from "./components/MyHeader";
-import MyNav from "./components/MyNav";
-import MyForm from "./components/MyForm";
-import Myfooter from "./components/Myfooter";
-import Myslider from "./components/Myslider";
+import Recomendados from "../components/Recomendados";
+import MyHeader from "../components/MyHeader";
+import MyNav from "../components/MyNav";
+import MyForm from "../components/MyForm";
+import Myfooter from "../components/Myfooter";
+import Myslider from "../components/Myslider";
 
 function App() {
     return (
