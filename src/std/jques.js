2c4d77d12fc1b587078cca86bfae4df4b9a54f16
Joan <joanques17@gmail.com>
2020-04-24 16:51:17 +0200

    props joan

--- src/std/jques.js
+++ src/std/jques.js
@@ -1,32 +1,135 @@
-import ReactDOM from "react-dom";
 import React from "react";
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-            customers: [{id:1,name:"Yuhu"},{id:2,name:"yuhu2"}]
-        };
-    }
-    render() {
-        const date = this.state.date;
-        const clients = this.state.customers;
-        const formatDate = date.toLocaleTimeString();
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {formatDate}.</h2>
-                <ul>
-                    {clients.map(i=>{
-                        return <li key={"Client"+i.id}>{i.name}</li>
-                    })}
-                </ul>
+import ReactDOM from "react-dom";
+import ComponetProps from "./2avoid/ComponetProps";
+import Card from "./2avoid/Card";
+const MyHeader = () => {
+    return <div>
+        <header className="row">
+            <div className="col-md-2">
+                <img src="/images/logo.png" height="68" width="60" alt="logo"/></div>
+            <nav className="col-md-6 navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">My web</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="col-sm-2">Contact 555-333210</div>
+            <div className="col-sm-2"><img src="/images/callus.jpg" height="40" width="40" alt="callus"/></div>
+        </header>
+    </div>
+};
+const MyMain = () => {
+    return <div>
+        <main className="row">
+            <article className="col-10">
+                <div className="row">
+                    <div className="col-8">
+                        <h2>Title</h2>
+                        <p><ComponetProps name={"Joan"}/></p>
+                        <section>
+                            <h3>title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid asperiores
+                                corporis dolor dolorem, dolorum ducimus expedita facilis libero molestiae nesciunt
+                                officiis
+                                perspiciatis quae repellendus, saepe totam vero. Delectus, ipsam!</p>
+                        </section>
+                        <section>
+                            <h3>title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid asperiores
+                                corporis dolor dolorem, dolorum ducimus expedita facilis libero molestiae nesciunt
+                                officiis
+                                perspiciatis quae repellendus, saepe totam vero. Delectus, ipsam!</p>
+                        </section>
+                        <section>
+                            <h3>title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid asperiores
+                                corporis dolor dolorem, dolorum ducimus expedita facilis libero molestiae nesciunt
+                                officiis
+                                perspiciatis quae repellendus, saepe totam vero. Delectus, ipsam!</p>
+                        </section>
+                        <section>
+                            <Card title={"Hola"} description={"holaaaaa"}/>
+                        </section>
+                    </div>
+                    <div className="col-4">
+                        <aside>
+                            <h3>title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa delectus fugiat
+                                illo
+                                illum ipsam iste, iure nam numquam officiis qui ullam unde velit. At dolor magni
+                                praesentium sed
+                                tempora.</p>
+                        </aside>
+                        <aside>
+                            <h3>title</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa delectus fugiat
+                                illo
+                                illum ipsam iste, iure nam numquam officiis qui ullam unde velit. At dolor magni
+                                praesentium sed
+                                tempora.</p>
+                        </aside>
+                    </div>
+                </div>
+            </article>
+            <div className="col-2">
+                <aside>
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias autem commodi dolorum ducimus
+                        eaque est fugit
+                        ipsum laboriosam porro voluptate! Aspernatur aut impedit labore laborum nobis quae recusandae
+                        reprehenderit,
+                        tempora.</p>
+                </aside>
+                <aside className="sidebar">&#160;</aside>
             </div>
-        );
-    }
-}
+        </main>
+    </div>
+};
+const MyFooter = () => {
+    return <div>
+        <footer className="row">
+            <nav className="col-md-6 navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">My web</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon">&#160;</span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="col-sm-2">Contact 555-333210</div>
+        </footer>
+    </div>
+};
 
+
+const MyApp = () => {
+    return <div>
+        <MyHeader/>
+        <MyMain/>
+        <MyFooter/>
+    </div>
+};
 const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<Clock/>, wrapper) : false;
-
+wrapper ? ReactDOM.render(<MyApp/>, wrapper) : false;
