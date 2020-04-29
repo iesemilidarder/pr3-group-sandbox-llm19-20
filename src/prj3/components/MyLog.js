b7ad224ae07c0f7a709f26f7db37b59fbca665aa
Daniel <dcs00@iesemilidarder.com>
2020-04-29 22:07:29 +0200

    Daniel Castaneda

--- src/prj3/components/MyLog.js
+++ src/prj3/components/MyLog.js
@@ -1,39 +1,48 @@
 import React from "react";
 import {useForm} from "react-hook-form";
 import "../myheader.css";
-
+import "../mylogin.css";
 
 function MyLog() {
     const {register, handleSubmit, watch, errors} = useForm();
+
+    let users= [
+        {
+            "id": 1,
+            "username": "Cioran666",
+            "password": "987654"
+        },
+        {
+            "id": 2,
+            "username": "Stoa33",
+            "password": "123987"
+        },
+        {
+            "id": 3,
+            "username": "Lory66",
+            "password": "123456"
+        }
+    ];
 
 
     const onSubmit = data => {
         console.log(data);
 
     };
-/*
-    let usuarios = [];
-
-    fetch("assets/prj3/data/userNames.json")
-        .then(data => data.json())
-        .then(users => {
-            usuarios = users.data;
-            console.log(usuarios);
-        });*/
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <ul>
                 <li>
                     <input placeholder={"Inserte el nombre de usuario"}/>
+                    {errors.exampleRequired && <p id="dis">"Este campo es requerido"</p>}
                     <input placeholder={"Inserte la contraseña"}
                            name="exampleRequired"
                            ref={register({ required: true, maxLength: 10 })}
-                    />
+                    />{errors.exampleRequired && <p>"Este campo es requerido"</p>}
                 </li>
                 <li>
                     <input style={{marginBottom: 25}} className="btn btn-primary" type="submit" value="Inicia sesión"/>
-                    {errors.exampleRequired && alert("Este campo es requerido")}
                     <input className="btn btn-secondary" type="button" value="Registrate"/>
 
                 </li>
