cededf228f11c38f5b2e618752731b8df6cdd1a7
Daniel <dcs00@iesemilidarder.com>
2020-04-30 10:50:19 +0200

    Daniel Castaneda

---
+++ src/prj3/components/MyAside.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../MyAside.css";
+
+const MyAside = () => {
+    return (
+        <div>
+            <aside>
+                <h2>Tendencias<h2>
+                    <ul>
+                        <section> </section>
+                        <section> </section>
+                    </ul>
+            </aside>
+
+        </div>
+);
+};
+
+export default MyAside;
