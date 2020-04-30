55a8699c79d8a6334907cc547c25df1c1403684c
Javier Zarco <jzb00@iesemilidarder.com>
2020-04-30 17:16:48 +0200

    .

--- src/prj4/Bodyprj4.js
+++ src/prj4/Bodyprj4.js
@@ -3,4 +3,3 @@ import PendienteDeUso from "../std/2avoid/PendienteDeUso";
 <aside className="col-4">
     <PendienteDeUso name={"Don Pimpon"}/>
 </aside>
-nk
--- src/prj4/prj4INDEX.js
+++ src/prj4/prj4INDEX.js
@@ -4,22 +4,40 @@ import NewsCard from "";
 import UpdateCard from "";
 
 <body className="bg-secondary">
-<header>
-    <!-- JS MenuBar -->
-</header>
+<div className="header">
+    <div className="container-fluid">
+        <nav className="row navbar navbar-dark bg-dark">
+            <div className="col-6">
+                <span className="navbar-brand text-success" href="#"><B>MUSIC</B></span>
+            </div>
+            <div className="col-6">
+                <form action="#" className="content float-right">
+                    <input type="submit" className="float-right bg-secondary border-dark"
+                           onClick="document.location='login.html'" value="Sign up"/>
+                    <input type="submit" className="float-right bg-success border-dark"
+                           onClick="document.location='login.html'" value="Log in"/>
+                </form>
+            </div>
+        </nav>
+    </div>
+</div>
 <main>
     <br>
-    <!-- JS SearchBar -->
     <br>
     <!-- JS GenedersButons -->
     <br>
     <br>
         <NewsCard/> <!-- JS NewsCards -->
     <br>
-        <UpdateCard/> <!-- JS UpdateCards -->
+        <UpdateCard/> </br>
+    </br>
+    </br>
+    </br>
+    </br> <!-- JS UpdateCards -->
 </main>
 <br>
 <footer>
     <!-- JS UnderBar -->
 </footer>
+</br>
 </body>
