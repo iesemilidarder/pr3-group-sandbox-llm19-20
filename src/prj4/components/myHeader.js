2515b80918134257de7879a4ab34bada41e35485
Javier Zarco <jzb00@iesemilidarder.com>
2020-05-07 17:50:33 +0200

    myHeader.js casi pasado a react

--- src/prj4/components/myHeader.js
+++ src/prj4/components/myHeader.js
@@ -1,2 +1,23 @@
 import React from "react";
 
+const myHeader = () => {
+    return <body className="bg-info">
+    <header id="myHeader">
+        <div className="container-fluid">
+            <nav className="row navbar navbar-dark bg-dark">
+                <div className="col-6">
+                    <span className="navbar-brand text-success" href="#"><B>MUSIC</B></span>
+                </div>
+                <div className="col-6">
+                    <form action="#" className="content float-right">
+                        <input type="submit" className="float-right bg-secondary border-dark"
+                               onClick="document.location='login.html'" value="Sign up"/>
+                        <input type="submit" className="float-right bg-success border-dark"
+                               onClick="document.location='login.html'" value="Log in"/>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </header>
+    </body>
+}
