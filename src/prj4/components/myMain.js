af0b1287d4193078df1734a58d3c05c86b6e1da6
Javier Zarco <jzb00@iesemilidarder.com>
2020-05-07 17:55:38 +0200

    Puesto el myMain.js e intento de hacer el myFooter.js
    Ninguno de todos los que he pasado están exportados al HTML!!!!!!

---
+++ src/prj4/components/myMain.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const myMain = () => {
+    return <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12" id="root"></div>
+            </div>
+            <div class="row">
+                <div class="col-10">
+                    <input class="form-control border-success" type="search" placeholder="Search by name of song or artist"
+                           aria-label="Search"/>
+                </div>
+                <div class="col-2">
+                    <button class="btn bg-success border border-dark" type="submit"
+                            onclick="document.location='Results.html'">Search
+                    </button>
+                </div>
+            </div>
+        </div>
+    </main>
+}
+
+export default myMain();
