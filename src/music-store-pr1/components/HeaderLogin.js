a0ac43b060224f3b2585225ee33c56349cf2330c
mikinario <mef01@iesemilidarder.com>
2020-05-17 18:40:21 +0200

    login

--- src/music-store-pr1/components/HeaderLogin.js
+++ src/music-store-pr1/components/HeaderLogin.js
@@ -19,30 +19,23 @@ export default function CutriLogin(props) {
         return (
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="row">
-                    <div className="col-sm">
-                        <img src="" alt="" className="mb-4"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <h1 className="h3 mb-3 font-weight-normal">
-                            Please sing in
-                        </h1>
-                    </div>
-                </div>
-                <div className="row">
                     <div className="col">
                         <label htmlFor="inputEmail" className="sr-only">
                             Email address
                         </label>
-                        <input  name="username" defaultValue="test" ref={register} id="inputEmail"
-                                className="form-control" placeholder="Email address" required autoFocus/>
+                        <input name="username" defaultValue="test" ref={register} id="inputEmail"
+                               className="form-control" placeholder="Email address" required autoFocus/>
                     </div>
                     <div className="col">
                         <label htmlFor="inputPassword" className="sr-only">Password</label>
                         <input name="password" ref={register({required: true})}
                                className="form-control" placeholder="Password" type="password" id="inputPassword"/>
                         {errors.password && <span>This field is required</span>}
+                    </div>
+                </div>
+                <div className="row" style={{paddingTop: "15px"}}>
+                    <div className="col">
+                        <input type="submit" value={"Do login!"} className="btn btn-lg btn-primary btn-block"/>
                     </div>
                 </div>
             </form>
