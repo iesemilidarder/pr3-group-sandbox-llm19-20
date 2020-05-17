7bf00d2009f8e6a7ffc94a5326f2b62a9d1ce8c7
Javier Zarco <jzb00@iesemilidarder.com>
2020-05-18 00:55:27 +0200

    Calificacion de 5 estrellas sin acabar.

--- src/music-store-pr1/components/TopicsFooter.js
+++ src/music-store-pr1/components/TopicsFooter.js
@@ -16,6 +16,14 @@ class TopicsFooter extends React.Component {
             <footer className="footer-copyright text-center py-3">
                  <p>Copyright 2020</p>
                 <p>Spotifi del Grupo 1 to guapo</p>
+                {/*//Para que voten nuestra pagina*/}
+                <div className="ec-stars-wrapper">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                </div>
             </footer>
         );
     }
