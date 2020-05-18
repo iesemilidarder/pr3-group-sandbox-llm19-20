e2593cf7b7c1212ee801ff82d194c3fcadbc20ea
Javier Zarco <jzb00@iesemilidarder.com>
2020-05-18 12:27:57 +0200

    .

--- src/music-store-pr1/components/TopicsMain.js
+++ src/music-store-pr1/components/TopicsMain.js
@@ -82,6 +82,12 @@ class TopicsMain extends React.Component {
     }
 
 
+    musicApi() {
+        fetch(`https://github.com/soundcloud/java-api-wrapper/tree/master/src/examples/java/com/soundcloud/api/examples`)
+        }
+
+
+
     render() {
         return (
             <div>
@@ -93,7 +99,7 @@ class TopicsMain extends React.Component {
 
                     </input>
                     <div>
-                        <button type="button" className="btn btn-outline-primary" data-toggle="button" aria-pressed="false" value="Click para ver mensaje">
+                        <button onClick={this.musicApi} type="button" className="btn btn-outline-primary" data-toggle="button" aria-pressed="false" value="Click para ver mensaje">
                             Buscar
                         </button>
                     </div>
